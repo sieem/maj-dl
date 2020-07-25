cfb319c10027ba2104050dfd85bcd04a29a0f413
Siem <siem.linx@gmail.com>
2020-07-25 18:55:16 +0200

    Write id3 tags

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { createWriteStream } from 'fs';
+import { createWriteStream, readFileSync, writeFileSync, unlinkSync } from 'fs';
 import { get } from 'https';
 import * as ytdl from 'ytdl-core';
+import { write } from 'ffmetadata';
+import * as moment from 'moment';
 import { google } from 'googleapis';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { config } from 'dotenv';
+
+config();
 
 // initialize the Youtube API library
 const youtube = google.youtube('v3');
@@ -17,7 +20,7 @@ const youtube = google.youtube('v3');
         maxResults: 1
     });
 
-    for (const {id: { videoId }, snippet: { thumbnails }} of items) {
+    for (const {id: { videoId }, snippet: { thumbnails, title, publishedAt }} of items) {
         // try to take the biggest one and go down from there
         const thumbnailUrl = thumbnails.maxres?.url || thumbnails.high?.url || thumbnails.medium?.url || thumbnails.standard?.url || thumbnails.default?.url;
 
@@ -34,17 +37,38 @@ const youtube = google.youtube('v3');
             }
         }
 
-        const tempAudioPath = `./tmp/${videoId}.${mimeTypeToDownload.includes('webm') ? 'opus' : 'mp4a'}`;
+        const fileExtension = mimeTypeToDownload.includes('webm') ? 'opus' : 'mp4a';
+
+        const tempAudioPath = `./tmp/${videoId}.${fileExtension}`;
         const tempThumbnailPath = `./tmp/${videoId}_thumb.jpg`;
 
         get(thumbnailUrl, res => res.pipe(createWriteStream(tempThumbnailPath)))
         const writeStream = createWriteStream(tempAudioPath);
 
-        writeStream.on('finish', function () {
-            console.log('done');
-        });
-
         ytdl(`http://www.youtube.com/watch?v=${videoId}`, { quality: qualityToDownload})
             .pipe(writeStream);
+
+        writeStream.on('finish', () => {
+            const album = title.includes('CBS') ? 'Coffee Break Sessions' : title.includes('Guest Mix') ? 'Guest Mix' : 'On Vinyl';
+            const tags = {
+                title,
+                album,
+                year: Number.parseInt(moment(publishedAt).format('YYYY')),
+                artist: 'My Analog Journal',
+                // APIC: tempThumbnailPath,
+            };
+
+            write(tempAudioPath, tags, function (err) {
+                if (err) console.error("Error writing metadata", err);
+                else {
+                    const finalAudioPath = `./done/${title.replace(/:/g, '-')}.${fileExtension}`;
+                    writeFileSync(finalAudioPath, readFileSync(tempAudioPath));
+                    unlinkSync(tempAudioPath);
+                    unlinkSync(tempThumbnailPath);
+                }
+            });
+
+            
+        });
     }
 })()
